6da892c772957e741555363795816828c6d6cb15
Brayden Morris <34358864+R3d3MpT10n101@users.noreply.github.com>
2019-03-28 14:10:08 -0700

    Update api.py

--- api.py
+++ api.py
@@ -1,11 +1,57 @@
-from flask import Flask, request
-import requests
+# import the Flask class from the flask module
+from flask import Flask, render_template, redirect, url_for, request
 
-#Create a Flask app instance
+
+# create the application object
 app = Flask(__name__)
 
 
+@app.route('/')
+def home():
+    return render_template('home.html')  # return a string
 
+@app.route('/signin', methods=['GET', 'POST'])
+def signin():
+
+  error = None
+  if request.method == 'POST':
+    if request.form['email'] != 'admin@gmail.com' or request.form['password'] != 'admin':
+        error = 'Invalid Credentials. Please try again.'
+    else:
+        return redirect(url_for('HighLowInput'))
+  return render_template('signin.html', error=error)
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+  error = None
+  #confirmpassword = request.form['confirmpassword']
+  #password = request.form['password']
+  if request.method == 'POST':
+    if request.form['confirmpassword'] != request.form['password']:
+        error = 'Your password doesn\'t match. Please try again.'
+    else:
+      return redirect(url_for('HighLowInput'))
+  return render_template('signup.html', error=error)  # render a template
+ # if request.method == 'POST':
+ # sign_up.sign_up(firstname=request.form['firstname'], lastname=request.form['lastname'], email=request.form['email'], password=request.form['password'], confirmpassword=request.form['confirmpassword'])
+
+@app.route('/resetPassword', methods=['GET', 'POST'])
+def resetPassword():
+  return render_template('resetPassword.html', methods=['GET', 'POST'])  # render a template
+
+@app.route('/input', methods=['GET', 'POST'])
+def HighLowInput():
+  error = None
+  if request.method == 'POST':
+    return redirect(url_for('delay'))
+    #return 'High: {} <br> Low: {}'.format(request.form['high'], request.form['low'])
+  return render_template('HighLow_Input.html', error=error)
+
+
+@app.route('/display', methods=['GET', 'POST'])
+def display():
+  if request.method == 'POST':
+    return 'High: {} <br> Low: {}'.format(request.form['high'], request.form['low']) 
 
 #Run the app
 if __name__ == '__main__':
